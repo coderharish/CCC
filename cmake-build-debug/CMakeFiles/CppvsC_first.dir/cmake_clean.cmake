08c06e36eb9f19db0c722f35e47f9468ea7a17d3
4year <harishg00@gmail.com>
2017-07-03 05:27:49 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/CppvsC_first.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppvsC_first.dir/main.cpp.o"
+  "CppvsC_first.pdb"
+  "CppvsC_first"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppvsC_first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
